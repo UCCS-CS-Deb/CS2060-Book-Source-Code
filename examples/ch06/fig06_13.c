00327db05d1a789e00e30f9dc564cb7b30f3762d
AmyJane <amyjane.reilly@gmail.com>
2022-06-27 14:57:44 -0600

    Update fig06_13.c

--- examples/ch06/fig06_13.c
+++ examples/ch06/fig06_13.c
@@ -1,16 +1,33 @@
 // Fig. 6.13: fig06_13.c
 // Passing arrays and individual array elements to functions.
 #include <stdio.h>
+#include <stdbool.h>
+
 #define SIZE 5
+#define REVIEWER 3
+#define MOVIE 4
+#define MIN_SCORE 0
+#define MAX_SCORE 10
 
 // function prototypes
 void modifyArray(int b[], size_t size);
-void modifyElement(int e);               
+void modifyElement(int e);        
+void printArray(const int c[], size_t size);
+void modifyMultiArray(int a[][MOVIE], size_t reviewer, size_t movie);
+void printMultiArray(const int c[][MOVIE], size_t reviewer, size_t movie);
 
 // function main begins program execution
 int main(void)
 {
    int a[SIZE] = { 0, 1, 2, 3, 4 }; // initialize array a
+   int multiArray[REVIEWER][MOVIE];
+
+   // Showing that the value of an array is the address of the first 
+   // element in the array 
+   puts("");
+   printf("array: %p    &array[0]: %p    &array: %p", a, &a[0], &a);
+   puts("");
+   puts("");
 
    puts("Effects of passing entire array by reference:\n\nThe "
       "values of the original array are:");
@@ -30,6 +47,7 @@ int main(void)
       printf("%3d", a[i]);
    } 
 
+
    // output value of a[3]
    printf("\n\n\nEffects of passing array element "
       "by value:\n\nThe value of a[3] is %d\n", a[3]);
@@ -38,6 +56,13 @@ int main(void)
 
    // output value of a[3]
    printf("The value of a[3] is %d\n", a[3]);
+
+
+   // Asking user for the values of the multi array
+   modifyMultiArray(multiArray, REVIEWER, MOVIE);
+
+   // Printing out the multi array
+   printMultiArray(multiArray, REVIEWER, MOVIE);
 }
 
 // in function modifyArray, "b" points to the original array "a" 
@@ -57,7 +82,56 @@ void modifyElement(int e)
    // multiply parameter by 2                                  
    printf("Value in modifyElement is %d\n", e *= 2);            
 } 
-                          
+                       
+// Printing an array
+void printArray(const int c[], size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        printf("Value of array[%llu]: %d\n", i, c[i]);
+    }
+}
+
+void modifyMultiArray(int a[][MOVIE], size_t reviewer, size_t movie) {
+    puts("");
+    //Initializing local variables
+    unsigned int score = 0;
+    int scanReturn = -1;
+    bool numberValid = false;
+
+    //for loop going through the multi array
+    for (size_t i = 0; i < reviewer; i++) {
+        for (size_t j = 0; j < movie; j++) {
+            //Asking user for the reviewer's score of the movie + validating outputs
+            numberValid = false;
+            do {
+                printf("Enter reviewer %llu's review of movie %llu (between %d-%d): ", i, j, MIN_SCORE, MAX_SCORE);
+                scanReturn = scanf("%u", &score);
+                while ((getchar()) != '\n');
+
+                // If scanf is not an int between 0-10
+                if (scanReturn != 1 || score < MIN_SCORE || score > MAX_SCORE) {
+                    puts("Invalid input :(");
+                }
+                else {
+                    a[i][j] = score;
+                    numberValid = true;
+                }
+            } while (!numberValid);
+            
+        }
+    }
+}
+
+void printMultiArray(const int c[][MOVIE], size_t reviewer, size_t movie) {
+    // Goes through rows
+    for (size_t i = 0; i < reviewer; i++) {
+        puts("");
+
+        // Goes through columns
+        for (size_t j = 0; j < movie; j++) {
+            printf("%d ", c[i][j]);
+        }
+    }
+}
 
 
 
